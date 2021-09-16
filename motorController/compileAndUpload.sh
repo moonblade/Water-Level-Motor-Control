d8295bdbc672c577411b07721db2a81f710f92e2
moonblade <moonblade168@gmail.com>
2021-09-16 20:31:42 +0530

    Simplify motor controller code

--- motorController/compileAndUpload.sh
+++ motorController/compileAndUpload.sh
@@ -1,2 +1,2 @@
-arduino-cli compile motorController --fqbn esp8266:esp8266:d1_mini
+arduino-cli compile motorController --fqbn esp8266:esp8266:d1_mini -e
 arduino-cli upload -p /dev/cu.usbserial-${1-143210} --fqbn esp8266:esp8266:d1_mini motorController.ino
--- motorController/motorController.ino
+++ motorController/motorController.ino
@@ -1,13 +1,20 @@
-#include "FirebaseESP8266.h"
+// Libraries
+#include <FirebaseESP8266.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
 
-#define FIREBASE_HOST "water-level-indicator-a555e-default-rtdb.firebaseio.com"  //Change to your Firebase RTDB project ID e.g. Your_Project_ID.firebaseio.com
-#define FIREBASE_AUTH "RSycUEGVNj1wiOVGrmXjQkdpE65voJNJmaGPs3Z7" //Change to your Firebase RTDB secret password
-#define WIFI_SSID "sarayi_2"
+// Firebase Auth
+#define FIREBASE_HOST "water-level-indicator-a555e-default-rtdb.firebaseio.com"  
+#define FIREBASE_AUTH "RSycUEGVNj1wiOVGrmXjQkdpE65voJNJmaGPs3Z7" 
+
+// Wifi details
 /* #define WIFI_SSID "Sarayi_ff_24" */
-#define WIFI_PASSWORD "code||die"
 /* #define WIFI_PASSWORD "abduljabbar" */
+#define WIFI_SSID "sarayi_2"
+#define WIFI_PASSWORD "code||die"
 
+// Defaults for operation
 #define ON D5
 #define ONTWO D6
 #define OFF D7
@@ -16,37 +23,16 @@
 #define TURNOFF LOW
 #define CACHESIZE 5
 
+// For auto updater
+const String CURRENT_VERSION = String("__BINARY_NAME:motorController__BINARY_VERSION:") + __DATE__ + __TIME__ + "__";
+const String BASE = String("/motorController");
+String latestVersion, downloadUrl;
 
 //Define Firebase Data objects
 FirebaseData fd;
-double timestamp, lastPercentageUpdate, lastMeasurementUpdate;
-int waitOnStartMin, switchingDelaySec, switchToDirectModeAfterMins;
-int measurement, minVal, maxVal, motorOffThreshold, motorOnThreshold;
-int inputVal;
-int shouldLog;
-int currentPercent;
-int lastFivePercent[CACHESIZE];
-int currentIndex = 0;
-String currentState, command;
-
-bool allLowerThan(int value) {
-  for (int i=0; i<CACHESIZE; i++) {
-    if (lastFivePercent[i] > value) {
-      return false;
-    }
-  }
-  return true;
-}
-
-void printlns(String statement) {
-  Serial.println(statement);
-  if (shouldLog == 1) {
-    Firebase.set(fd, "/logs/" + String(millis()), statement);
-  }
-}
+String currentState = String("off");
 
 void setup() {
-  shouldLog = 0;
   pinMode(ON, OUTPUT);
   pinMode(ONTWO, OUTPUT);
   pinMode(OFF, OUTPUT);
@@ -69,42 +55,30 @@ void setup() {
 
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
-
-  // On start wait five minutes before doing anything
-  Firebase.getInt(fd, "settings/waitOnStartMin");
-  waitOnStartMin = fd.intData();
-  Firebase.getInt(fd, "settings/switchingDelaySec");
-  switchingDelaySec = fd.intData();
-  currentState = "off";
-  delay(waitOnStartMin*60*1000);
-
-  for (int i=0; i<CACHESIZE; ++i) {
-    lastFivePercent[CACHESIZE] = 50;
-  }
 }
 
 void motorOn() {
-  printlns("Turning motor on");
-  printlns("Button on");
+  Serial.println("Turning motor on");
+  Serial.println("Button on");
   digitalWrite(ON, TURNON);
   digitalWrite(ONTWO, TURNON);
-  delay(switchingDelaySec * 1000);
-  printlns("Button off");
+  delay(1000);
+  Serial.println("Button off");
   digitalWrite(ON, TURNOFF);
   digitalWrite(ONTWO, TURNOFF);
 }
 
 void motorOff() {
-  printlns("Turning motor off");
-  printlns("Button on");
+  Serial.println("Turning motor off");
+  Serial.println("Button on");
   digitalWrite(OFF, TURNON);
-  delay(switchingDelaySec * 1000);
-  printlns("Button off");
+  delay(1000);
+  Serial.println("Button off");
   digitalWrite(OFF, TURNOFF);
 }
 
-void readCurrentState() {
-  inputVal = digitalRead(CURR);
+void setCurrentState() {
+  int inputVal = digitalRead(CURR);
   if (inputVal == 1) {
     currentState = "on";
   } else {
@@ -112,23 +86,41 @@ void readCurrentState() {
   }
 
   // set current state to firebase
-  printlns("Current state " + currentState);
-  Firebase.set(fd, "/motorController/state/current", currentState);
-  Firebase.setTimestamp(fd, "/motorController/state/timestamp");
+  Serial.println("Current state " + currentState);
+  Firebase.set(fd, BASE + "/state/current", currentState);
+  Firebase.setTimestamp(fd, BASE + "/state/timestamp");
 }
 
-void initVariables() {
-  Firebase.getString(fd, "/motorController/command/current");
-  command = fd.stringData();
+int getCurrentPercentage() {
+  Firebase.getInt(fd, "/waterLevelSensor/output/percentage");
+  return fd.intData();
+}
 
-  Firebase.getInt(fd, "/settings/motorControllerLog");
-  shouldLog = fd.intData();
- 
+String getCommand(int percentage) {
+  Firebase.getInt(fd, BASE + "/configuration/motorOffThreshold");
+  int motorOffThreshold = fd.intData();
+
+  Firebase.getInt(fd, BASE + "/configuration/motorOnThreshold");
+  int motorOnThreshold = fd.intData();
+
+  String command = "none";
+  if (percentage > motorOffThreshold) {
+    command = "off";
+  } else if (percentage < motorOnThreshold) {
+    command = "on";
+  } else {
+    command = "none";
+  }
+  Serial.println(String("Direct mode command is: ") + command);
+  return command;
 }
 
 void controlMotor() {
+  int percentage = getCurrentPercentage();
+
+  String command = getCommand(percentage);
   if (command != currentState && command != "none") {
-    printlns("Turning motor " + command);
+    Serial.println("Turning motor " + command);
     if (command == "on") {
       motorOn();
     } else if (command == "off"){
@@ -137,71 +129,30 @@ void controlMotor() {
   }
 }
 
-bool switchOverToManualMode() {
-  Firebase.getDouble(fd, "/waterlevel/percentageUpdatedAt");
-  lastPercentageUpdate = fd.doubleData();
-
-
-  Firebase.getDouble(fd, "/waterlevel/timestamp");
-  lastMeasurementUpdate = fd.doubleData();
-
-  Firebase.getInt(fd, "/settings/switchToDirectModeAfterMins");
-  switchToDirectModeAfterMins = fd.intData();
-
-  return (lastMeasurementUpdate - lastPercentageUpdate > switchToDirectModeAfterMins * 60 * 1000);
+void updateFirmware(String firmwareUrl) {
+  WiFiClientSecure client;
+  client.setInsecure();
+  ESPhttpUpdate.update(client, firmwareUrl); 
 }
 
-void calculatePercentageAndAddToList() {
-    //find currentPercent
-    Firebase.getInt(fd, "/waterlevel/measurement");
-    measurement = fd.intData();
-    Firebase.getInt(fd, "settings/maximumValue");
-    maxVal = fd.intData();
-    Firebase.getInt(fd, "settings/minimumValue");
-    minVal = fd.intData();
+void checkForUpdates() {
+  Firebase.set(fd, BASE + "/firmware/currentVersion", CURRENT_VERSION);
+  Firebase.getString(fd, BASE + "/firmware/binaryVersion");
+  latestVersion = fd.stringData();
 
-    currentPercent = max(min(100, (100 - (((measurement - minVal) * 100) / max((maxVal - minVal), 1)))), 0);
-
-    //Add it to list
-    lastFivePercent[currentIndex] = currentPercent;
-    currentIndex += 1;
-    currentIndex %= CACHESIZE;
-}
-
-void createCurrentCommand() {
-  Firebase.getInt(fd, "settings/motorOffThreshold");
-  motorOffThreshold = fd.intData();
-
-  Firebase.getInt(fd, "settings/motorOnThreshold");
-  motorOnThreshold = fd.intData();
-
-  if (currentPercent > motorOffThreshold) {
-    command = "off";
-  } else if (allLowerThan(motorOnThreshold)) {
-    command = "on";
-  } else {
-    command = "none";
+  if (!latestVersion.equalsIgnoreCase(CURRENT_VERSION)) {
+    Serial.println("Version mismatch, downloading update");
+    Firebase.getString(fd, BASE + "/firmware/downloadUrl");
+    downloadUrl = fd.stringData();
+    updateFirmware(downloadUrl);
   }
-  printlns("Direct mode command is: " + command);
-}
-
-void controlMotorInManualMode() {
-  printlns("Taking over motor control in direct mode");
-  calculatePercentageAndAddToList();
-  createCurrentCommand();
-  controlMotor();
 }
 
 void loop() {
-  readCurrentState();
-
-  initVariables();
+  setCurrentState();
 
   controlMotor();
 
-  if (switchOverToManualMode()) {
-    controlMotorInManualMode();
-  }
-
+  checkForUpdates();
   delay(10000);
 }
